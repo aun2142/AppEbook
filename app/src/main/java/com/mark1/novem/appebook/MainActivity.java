ed7c6b60a1f1e7f96a65e2c46ab2656361e294d7
chanon <November_Young@hotmail.com>
2016-09-06 11:17:18 +0700

    startapp

---
+++ app/src/main/java/com/mark1/novem/appebook/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mark1.novem.appebook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }   // Main Method
+
+}   // Main Class
